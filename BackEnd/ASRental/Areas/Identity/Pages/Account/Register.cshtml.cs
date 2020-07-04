96aeb8e6a4308a59c167f887a1b9d69e14ee7ebc
Andrei Negreț <andrei.negret@yahoo.com>
2020-07-04 22:52:41 +0300

    Adding style to Register and login

--- BackEnd/ASRental/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BackEnd/ASRental/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -97,7 +97,7 @@ namespace ASRental.Areas.Identity.Pages.Account
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email});
                     }
                     else
                     {
